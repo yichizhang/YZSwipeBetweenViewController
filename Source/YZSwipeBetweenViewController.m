72734490bdf1d39514e63cfd53547f996295cf4e
Yichi Zhang <zhang-yi-chi@hotmail.com>
2016-04-10 07:38:50 +1000

    Use 4 spaces for indentation instead of tab

--- Source/YZSwipeBetweenViewController.m
+++ Source/YZSwipeBetweenViewController.m
@@ -1,15 +1,15 @@
 /*
- 
+
  Copyright (c) 2014 Yichi Zhang
  https://github.com/yichizhang
  zhang-yi-chi@hotmail.com
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
+
  */
 
 #import "YZSwipeBetweenViewController.h"
@@ -23,8 +23,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-	[self setupViewControllersForScrollView];
-	
+    [self setupViewControllersForScrollView];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,110 +34,101 @@
 
 #pragma mark - Private methods
 - (void)removeViewControllersFromScrollView{
-	
-	for (UIViewController *vc in self.viewControllers) {
-		
-		[vc willMoveToParentViewController:nil];
-		[vc.view removeFromSuperview];
-		[vc removeFromParentViewController];
-		
-	}
-	
+
+    for (UIViewController *vc in self.viewControllers) {
+
+        [vc willMoveToParentViewController:nil];
+        [vc.view removeFromSuperview];
+        [vc removeFromParentViewController];
+
+    }
+
 }
 
 - (void)addViewControllersToScrollView{
-	
-	[self.scrollView removeFromSuperview];
-	
-	CGRect mainScreenBounds = [[UIScreen mainScreen] bounds];
-	
-	CGFloat currentOriginX = 0;
-	
-	for (UIViewController *vc in self.viewControllers) {
-		CGRect frame = vc.view.frame;
-		frame.origin.x = currentOriginX;
-		vc.view.frame = frame;
-		
-		[self addChildViewController:vc];
-		[self.scrollView addSubview:vc.view];
-		[vc didMoveToParentViewController:self];
-		
-		currentOriginX += mainScreenBounds.size.width;
-	}
-	
-	self.scrollView.contentSize =
-	CGSizeMake(
-			   currentOriginX,
-			   mainScreenBounds.size.height
-			   );
-	self.scrollView.pagingEnabled = YES;
-	self.scrollView.showsHorizontalScrollIndicator = NO;
-	self.scrollView.showsVerticalScrollIndicator = NO;
-	
-	[self scrollToViewControllerAtIndex:self.initialViewControllerIndex];
-	
-	[self.view addSubview:self.scrollView];
-	
+
+    [self.scrollView removeFromSuperview];
+
+    CGRect mainScreenBounds = [[UIScreen mainScreen] bounds];
+
+    CGFloat currentOriginX = 0;
+
+    for (UIViewController *vc in self.viewControllers) {
+        CGRect frame = vc.view.frame;
+        frame.origin.x = currentOriginX;
+        vc.view.frame = frame;
+
+        [self addChildViewController:vc];
+        [self.scrollView addSubview:vc.view];
+        [vc didMoveToParentViewController:self];
+
+        currentOriginX += mainScreenBounds.size.width;
+    }
+
+    self.scrollView.contentSize =
+    CGSizeMake(
+               currentOriginX,
+               mainScreenBounds.size.height
+               );
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    self.scrollView.showsVerticalScrollIndicator = NO;
+
+    [self scrollToViewControllerAtIndex:self.initialViewControllerIndex];
+
+    [self.view addSubview:self.scrollView];
+
 }
 
 - (void)setupViewControllersForScrollView{
-	
-	[self removeViewControllersFromScrollView];
-	[self addViewControllersToScrollView];
-	
+
+    [self removeViewControllersFromScrollView];
+    [self addViewControllersToScrollView];
+
 }
 
 #pragma mark - Public Methods
 - (void)setViewControllers:(NSArray *)viewControllers{
-	
-	_viewControllers = viewControllers;
-	
-	[self setupViewControllersForScrollView];
-	
+
+    _viewControllers = viewControllers;
+
+    [self setupViewControllersForScrollView];
+
 }
 
 - (void)reloadViewControllers{
-	
-	[self setupViewControllersForScrollView];
-	
+
+    [self setupViewControllersForScrollView];
+
 }
 
 - (void)scrollToViewControllerAtIndex:(NSInteger)index{
-	
-	[self scrollToViewControllerAtIndex:index animated:NO];
+
+    [self scrollToViewControllerAtIndex:index animated:NO];
 
 }
 
 - (void)scrollToViewControllerAtIndex:(NSInteger)index animated:(BOOL)animated{
 
-	if (index >= 0 && index < self.viewControllers.count) {
-		[self.scrollView
-		 scrollRectToVisible:[self.viewControllers[index] view].frame
-		 animated:animated
-		 ];
-	}
+    if (index >= 0 && index < self.viewControllers.count) {
+        [self.scrollView
+         scrollRectToVisible:[self.viewControllers[index] view].frame
+         animated:animated
+         ];
+    }
 
 }
 
 #pragma mark - Lazy loading of members
 - (UIScrollView *)scrollView{
-	
-	if (!_scrollView) {
-		_scrollView = [[UIScrollView alloc] initWithFrame:
-					   [[UIScreen mainScreen] bounds]
-					   ];
-	}
-	return _scrollView;
-	
-}
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if (!_scrollView) {
+        _scrollView = [[UIScrollView alloc] initWithFrame:
+                       [[UIScreen mainScreen] bounds]
+                       ];
+    }
+    return _scrollView;
+
 }
-*/
 
 @end
--- YZSwipeBetweenViewControllerDemo/YZSwipeBetweenViewControllerDemo/AppDelegate.m
+++ YZSwipeBetweenViewControllerDemo/YZSwipeBetweenViewControllerDemo/AppDelegate.m
@@ -1,15 +1,15 @@
 /*
- 
+
  Copyright (c) 2014 Yichi Zhang
  https://github.com/yichizhang
  zhang-yi-chi@hotmail.com
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
+
  */
 
 #import "AppDelegate.h"
@@ -24,59 +24,59 @@
 
 - (BOOL)application:(UIApplication *)application
 didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	
-	// Override point for customization after application launch.
-	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-	self.window.backgroundColor = [UIColor blackColor];
-	
-	self.swipeBetweenVC = [YZSwipeBetweenViewController new];
-	[self setupRootViewControllerForWindow];
-	self.window.rootViewController = self.swipeBetweenVC;
-	
-	[self.window makeKeyAndVisible];
-	
-	return YES;
+
+    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor blackColor];
+
+    self.swipeBetweenVC = [YZSwipeBetweenViewController new];
+    [self setupRootViewControllerForWindow];
+    self.window.rootViewController = self.swipeBetweenVC;
+
+    [self.window makeKeyAndVisible];
+
+    return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 - (void)setupRootViewControllerForWindow{
-	
-	UIViewController *vc1 = [DemoMainViewController new];
-	UINavigationController *navCon1 =
-	[[UINavigationController alloc]initWithRootViewController:vc1];
-	
-	UIViewController *vc2 = [DemoMainViewController new];
-	UINavigationController *navCon2 =
-	[[UINavigationController alloc] initWithRootViewController:vc2];
-	
-	UIViewController *vc3 = [DemoMainViewController new];
-	UINavigationController *navCon3 =
-	[[UINavigationController alloc] initWithRootViewController:vc3];
-	
-	self.swipeBetweenVC.viewControllers = @[navCon1, navCon2, navCon3];
-	self.swipeBetweenVC.initialViewControllerIndex = (NSInteger)self.swipeBetweenVC.viewControllers.count/2;
-	
+
+    UIViewController *vc1 = [DemoMainViewController new];
+    UINavigationController *navCon1 =
+    [[UINavigationController alloc]initWithRootViewController:vc1];
+
+    UIViewController *vc2 = [DemoMainViewController new];
+    UINavigationController *navCon2 =
+    [[UINavigationController alloc] initWithRootViewController:vc2];
+
+    UIViewController *vc3 = [DemoMainViewController new];
+    UINavigationController *navCon3 =
+    [[UINavigationController alloc] initWithRootViewController:vc3];
+
+    self.swipeBetweenVC.viewControllers = @[navCon1, navCon2, navCon3];
+    self.swipeBetweenVC.initialViewControllerIndex = (NSInteger)self.swipeBetweenVC.viewControllers.count/2;
+    
 }
 
 @end
